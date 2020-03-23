b84f71949d10c3b1e93971230f70232ff140f0ae
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-23 14:32:48 -0600

    12. Adding function in Board

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -42,4 +42,30 @@ defmodule IslandsEngine.Board do
 
   defp guess_response(:miss, board), do: {:miss, :none, :no_win, board}
 
+  defp forest_check(board, key) do
+    case forested?(board, key) do
+      true -> key
+      false -> :none
+    end
+  end
+
+  defp forested?(board, key) do
+    board
+    |> Map.fetch!(key)
+    |> Island.forested?()
+  end
+
+  defp win_check(board) do
+    case all_forested?(board) do
+      true -> :win
+      false -> :no_win
+    end
+  end
+
+  defp all_forested?(board) do
+    Enum.all?(board, fn {_key, island} ->
+      Island.forested?(island)
+    end)
+  end
+
 end
