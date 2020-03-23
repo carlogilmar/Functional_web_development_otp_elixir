922ad9a1de6c50810b303e22d6fe17f76b130a56
Carlo Gilmar <carlogilmar12@gmail.com>
2020-03-23 14:27:43 -0600

    10. Adding functions in Board

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -1,4 +1,38 @@
 defmodule IslandsEngine.Board do
+  alias IslandsEngine.Island
 
   def new(), do: %{}
+
+  def position_island(board, key, %Island{} = island) do
+    case overlaps_existing_island?(board, key, island) do
+      true -> {:error, :overlapping_island}
+      false -> Map.put(board, key, island)
+    end
+  end
+
+  defp overlaps_existing_island?(board, new_key, new_island) do
+    Enum.any?(board, fn {key, island} ->
+      key != new_key and Island.overlaps?(island, new_island)
+    end)
+  end
+
+  def all_islands_positiones?(board) do
+    Enum.all?(Island.types, &(Map.has_key?(board, &1)))
+  end
+
+  def guess(board, %Coordinate{} = coordinate) do
+    board
+    |> check_all_islands(coordinate)
+    |> guess_response(board)
+  end
+
+  defp check_all_islands(board, coordinate) do
+    Enum.find_value(board, :miss, fn {key, island} ->
+      case Island.guess(island, coordinate) do
+        {:hit, island} -> {key, island}
+        :miss -> false
+      end
+    end)
+  end
+
 end
